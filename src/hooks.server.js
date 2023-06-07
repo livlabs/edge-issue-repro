5a8095ee1742e1a52d46882efbf505faefe6b88e
brittian <bjwarner13@gmail.com>
2023-06-07 00:01:44 -0400

    fix up

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,28 +1,18 @@
 import Iron from "@hapi/iron";
 
-const password = "some_not_random_password_that_is_at_least_32_characters";
-
-async function decrypt(data) {
-  return (
-    data &&
-    Iron.unseal(
-      data,
-      "wqkljefbqkwjebfkjqwebfjkwqebfkjwqbefkjqwbefkjwqbefkjqwbefkjbwef",
-      Iron.defaults
-    )
-  );
-}
-
-const obj = {
-  username: "JohnDoe",
-  role: "admin",
-};
-
-const sealed = await Iron.seal(obj, password, Iron.defaults);
-
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve, cookie }) {
   //Get Cookie
+
+  const password = "some_not_random_password_that_is_at_least_32_characters";
+
+  const obj = {
+    username: "JohnDoe",
+    role: "admin",
+  };
+
+  const sealed = await Iron.seal(obj, password, Iron.defaults);
+
   //Decrypt Cookie
   const unsealed = await Iron.unseal(
     decodeURIComponent(encodeURIComponent(sealed)),
